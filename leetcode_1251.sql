78c2779790928e2b2b23ea8050bdfa25b7edfd59
Srikrishna <krishpullabhatla777@gmail.com>
2025-03-27 23:12:52 -0500

    Day 3

---
+++ leetcode_1251.sql
@@ -0,0 +1,27 @@
+# Approach 1
+
+# Write your MySQL query statement below
+
+SELECT 
+    p.product_id,
+    ROUND(IFNULL(SUM(p.price * u.units) / NULLIF(SUM(u.units), 0), 0), 2) AS average_price
+FROM
+    prices p
+LEFT JOIN
+    unitssold u ON p.product_id = u.product_id AND u.purchase_date BETWEEN p.start_date AND p.end_date
+GROUP BY
+    p.product_id;
+
+
+# Approach 2
+
+SELECT 
+    p.product_id,
+    ROUND(SUM(p.price * u.units) / SUM(u.units), 2) AS average_price
+FROM 
+    Prices p
+JOIN 
+    UnitsSold u ON p.product_id = u.product_id 
+    AND u.purchase_date BETWEEN p.start_date AND p.end_date
+GROUP BY 
+    p.product_id;
